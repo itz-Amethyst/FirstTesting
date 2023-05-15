227f4e68985a5fb1a980b722b2170d39d049fcdf
itz-amethyst <pransermi@gmail.com>
2023-05-16 00:15:46 +0430

    feat: Added another test

--- FirstTesting/EvaluatorTest.cs
+++ FirstTesting/EvaluatorTest.cs
@@ -14,5 +14,17 @@ namespace FirstTesting
 
             Assert.Equal("Even", actual);
         }
+
+        [Fact]
+        public void Evaluate_Should_Return_Odd()
+        {
+            const int input = 10;
+
+            var evaluator = new Evaluator();
+
+            var actual = evaluator.Evaluate(input);
+
+            Assert.Equal("Odd", actual);
+        }
     }
 }
