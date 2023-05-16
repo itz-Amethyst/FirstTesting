58de2a9d47acdd5037c49f6297dae8f5cb050287
itz-amethyst <pransermi@gmail.com>
2023-05-16 13:14:49 +0430

    feat: added more test

--- FirstTesting/FizzBuzz/FizzBuzzTest.cs
+++ FirstTesting/FizzBuzz/FizzBuzzTest.cs
@@ -13,5 +13,38 @@ namespace FirstTesting.FizzBuzz
 
             Assert.Equal(rounds , actualResult.Count);
         }
+
+        [Fact]
+        public void Start_ShouldReturnAListWithFizzValue()
+        {
+            const int rounds = 100;
+
+            var actual = FizzBuzz.Start(rounds);
+
+            Assert.Equal("Fizz" , actual[2]);
+            Assert.Equal("Fizz", actual[8]);
+        }
+
+        [Fact]
+        public void Start_ShouldReturnAListWithBuzzValue()
+        {
+            const int rounds = 100;
+
+            var actual = FizzBuzz.Start(rounds);
+
+            Assert.Equal("Buzz", actual[4]);
+            Assert.Equal("Buzz", actual[9]);
+        }
+
+        [Fact]
+        public void Start_ShouldReturnAListWithFizzBuzzValue()
+        {
+            const int rounds = 100;
+
+            var actual = FizzBuzz.Start(rounds);
+
+            Assert.Equal("FizzBuzz", actual[14]);
+            Assert.Equal("FizzBuzz", actual[29]);
+        }
     }
 }
