ccb7f2ea1502cad0f079953270ed976e1e7e492b
itz-amethyst <pransermi@gmail.com>
2023-08-03 10:54:57 +0330

    feat: ClassFixture

---
+++ Academic.Domain.Tests/ClassFixtures/IdentifierFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace Academic.Domain.Tests.ClassFixtures
+{
+    public class IdentifierFixture : IDisposable
+    {
+        public static Guid Id { get; set; }
+
+        public IdentifierFixture()
+        {
+            Id = Guid.NewGuid();
+        }
+
+
+        public void Dispose()
+        {
+            Id = Guid.Empty;
+        }
+    }
+}
